3438351903d6ece41a8c697a9974ad6f9305637e
mdomnik <mdomnik@vbox.fritz.box>
2025-07-16 19:12:12 +0200

    bug fixing and password control

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-service mariadb start
+# Read secrets from mounted files
+MYSQL_PASSWORD=$(cat /run/secrets/db_password.txt)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
 
+# Start service
+service mariadb start
 sleep 5
 
-# Run SQL commands properly
+# SQL Setup
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Graceful shutdown and restart in safe mode
+# Shut down gracefully and restart in foreground
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
-
 exec mysqld_safe
