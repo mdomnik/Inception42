54ea2c00c3c585bf2786cfed75541e28316ed001
Maciej Domnik <mdomnik.maciej@gmail.com>
2025-09-08 13:56:22 +0200

    new version

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -45,7 +45,7 @@ done
 if ! wp core is-installed --allow-root; then
   wp core install     --url="https://${DOMAIN_NAME:-localhost}"     --title="${SITE_TITLE}"     --admin_user="${WP_ADMIN_USER}"     --admin_password="${WP_ADMIN_PASSWORD}"     --admin_email="${WP_ADMIN_EMAIL}"     --skip-email     --allow-root
 
-  wp user create     "${WP_USER}" "${WP_USER_EMAIL}"     --user_pass="${WP_USER_PASSWORD}"     --role=editor     --allow-root
+  wp user create     "${WP_USER}" "${WP_USER_EMAIL}"     --user_pass="${WP_USER_PASSWORD}"     --role=subscriber     --allow-root
 fi
 
 mkdir -p /run/php
